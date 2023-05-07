aaf57dfa33cd6f6bc3b090ba165e94c7c9504301
Emmanuel Frécon <efrecon@gmail.com>
2023-05-07 13:37:57 +0200

    Export DEBIAN_FRONTEND for all dependencies

--- tcl/docker/dependencies.sh
+++ tcl/docker/dependencies.sh
@@ -5,11 +5,14 @@ set -eu
 # shellcheck source=../../share/dinosaurs/utils.sh
 . "$(dirname "$0")/../share/dinosaurs/utils.sh"
 
+# Turn down warnings about non-interactive mode
+DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND
 
 SRC_LIST=$(mktemp)
 sed s/archive/old-releases/g /etc/apt/sources.list > "$SRC_LIST"
 if_sudo mv "$SRC_LIST" /etc/apt/sources.list
 if_sudo apt-get update
-DEBIAN_FRONTEND=noninteractive if_sudo apt-get upgrade -yy
-DEBIAN_FRONTEND=noninteractive if_sudo apt-get install -y \
+if_sudo apt-get upgrade -yy
+if_sudo apt-get install -y \
     build-essential autoconf gcc-multilib g++-multilib libc6-dev-i386 libtool
