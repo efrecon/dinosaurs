a1ed1093984674303336e56d20d72ece94b6342c
Emmanuel Frécon <efrecon@gmail.com>
2023-05-31 23:24:30 +0200

    Remove MS source

--- tk/docker/dependencies.sh
+++ tk/docker/dependencies.sh
@@ -11,6 +11,7 @@ export DEBIAN_FRONTEND
 
 repoint_sources_list
 #if_sudo dpkg --add-architecture i386
+if_sudo rm -f /etc/apt/sources.list.d/microsoft-prod.list
 if_sudo apt-get update
 if_sudo apt-get install -y \
     build-essential autoconf gcc-multilib libc6-dev-i386 libtool libx11-dev
